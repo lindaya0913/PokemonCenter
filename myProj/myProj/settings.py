756f5a76bc14072999823876708bff991d6e8fa8
lindaya0913 <lindy0913@gmail.com>
2021-07-26 20:00:14 +0800

    secret

--- myProj/myProj/settings.py
+++ myProj/myProj/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lindy0913@gmail.com'
-EMAIL_HOST_PASSWORD = '(31522321lL)'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
