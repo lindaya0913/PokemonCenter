02c5dbe7277a41901fe31c444fddc9710c59dce0
lindaya0913 <lindy0913@gmail.com>
2021-07-26 19:19:03 +0800

    sendind email func

--- myProj/myProj/settings.py
+++ myProj/myProj/settings.py
@@ -125,3 +125,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lindy0913@gmail.com'
+EMAIL_HOST_PASSWORD = '(31522321lL)'
--- myProj/products_app/views.py
+++ myProj/products_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import OrderList
 from django.contrib import messages
-from shop_app import views as shop
+from shop_app import views as shop_view
+from shop_app import models as shop_model
+from django.core.mail import send_mail
+from django.conf import settings
 
 def product_list(request):
     products = {'closing': ['Pikachu','Ditto'],
@@ -16,9 +19,17 @@ def order(request):
     if request.method == 'POST':
         prod_list = request.POST.getlist('products')
         prod_str = ','.join(prod_list)
-        order_data = OrderList(wholelist=prod_str, username=shop.username)
+        order_data = OrderList(wholelist=prod_str, username=shop_view.username)
         order_data.save()
-        messages.success(request, 'Order created successfully:' + prod_str)
-        return redirect('loggedin')
+        user_data = shop_model.RegisteredUser.objects.get(name=shop_view.username)
+        recipientlist = [user_data.email,]
+        send_mail(
+            'Order from Pokémon Center',
+            'Hi, \n \n Below are the products that you have orderd from Pokémon Center. \n \n {}'.format(prod_str),
+            settings.EMAIL_HOST_USER,
+            recipientlist
+        )
+        messages.success(request, 'Order has been created successfully and a mail with the list of products has been sent to your registered email address')
+        return render(request, 'ordersuccess.html')
     else:
         return render(request, 'product_list.html')
